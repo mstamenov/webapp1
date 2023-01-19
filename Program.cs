53a47ac63b7ffcf11cff65be94e43bec181e6cfc
mstamenov <mstamenov@david.bg>
2023-01-19 07:35:51 +0000

    Remove space in title

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Registered to mstamenov @ 2023");
+app.MapGet("/", () => "Registered to mstamenov @2023");
 
 app.Run();
